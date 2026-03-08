4e762a97b96bf579bec9dac4a02c59fd28163921
Elad <Elad204@gmail.com>
2026-03-08 18:47:07 -0400

    Adding Array for Cards

--- scripts/index.js
+++ scripts/index.js
@@ -17,6 +17,32 @@ const saveProfileForm = editProfileModal.querySelector("#profile-form");
 const inputImagelink = addNewPostModal.querySelector("#card-image-input");
 const inputCaption = addNewPostModal.querySelector("#card-caption-input");
 const savePostForm = addNewPostModal.querySelector("#post-form");
+let initialCards = [
+  {
+    name: "Val Thorens",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/1-photo-by-moritz-feldmann-from-pexels.jpg",
+  },
+  {
+    name: "Restaurant terrace",
+    Link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/2-photo-by-ceiline-from-pexels.jpg",
+  },
+  {
+    name: "An outdoor cafe",
+    Link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/3-photo-by-tubanur-dogan-from-pexels.jpg",
+  },
+  {
+    name: "A very long bridge, over the forest and through the trees",
+    Link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/4-photo-by-maurice-laschet-from-pexels.jpg",
+  },
+  {
+    name: "Tunnel with morning light",
+    Link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/5-photo-by-van-anh-nguyen-from-pexels.jpg",
+  },
+  {
+    name: "Mountain house",
+    Link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/6-photo-by-moritz-feldmann-from-pexels.jpg",
+  },
+];
 
 function handleProfileFormSubmit(evt) {
   evt.preventDefault();
@@ -58,3 +84,7 @@ closeBtnPost.addEventListener("click", function () {
 saveProfileForm.addEventListener("submit", handleProfileFormSubmit);
 
 savePostForm.addEventListener("submit", handlePostFormSubmit);
+
+initialCards.forEach(function (card) {
+  console.log(card);
+});
